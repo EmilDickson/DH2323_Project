2ec07b92ff72823f7a23377850dc1679d254c6e5
EmilDickson <emil.dickson@gmail.com>
2019-05-09 13:24:32 +0200

    Inverted directions for movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -15,27 +15,27 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(speed * Time.deltaTime,0,0);
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(-speed * Time.deltaTime,0,0);
         }
-        if(Input.GetKey(KeyCode.Q))
+        if(Input.GetKey(KeyCode.E))
         {
             transform.position += new Vector3(0,-speed * Time.deltaTime,0);
         }
-        if(Input.GetKey(KeyCode.E))
+        if(Input.GetKey(KeyCode.Q))
         {
             transform.position += new Vector3(0,speed * Time.deltaTime,0);
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0,0,-speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0,0,speed * Time.deltaTime);
         }
